e2638a20c2f99e8122c91475b8a13186d72fd3c0
CJcode6754 <ceejayibabiosa@gmail.com>
2025-06-17 17:13:15 +0800

    feat: add animated movies

--- src/Pages/Anime.jsx
+++ src/Pages/Anime.jsx
@@ -1,7 +1,76 @@
-import React from 'react'
+import React from "react";
+import AnimeCard from "../components/AnimeCard";
+import { useCallback, useEffect, useState } from "react";
+import { useDebounce } from "react-use";
+import Loading from "../components/Loading";
+import { useOutletContext } from "react-router-dom";
+import { fetchAnimatedByQuery, fetchPopularAnimated } from "../utils/api";
 
 export default function Anime() {
+  const { searchTerm } = useOutletContext();
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState("");
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [animatedList, setAnimatedList] = useState([]);
+
+  useDebounce(
+    () => {
+      setDebouncedSearchTerm(searchTerm);
+    },
+    800,
+    [searchTerm]
+  );
+
+  const fetchAnimated = useCallback(async (query = "") => {
+    setIsLoading(true);
+    setErrorMessage(null);
+
+    try {
+      const data = query
+        ? await fetchAnimatedByQuery(query)
+        : await fetchPopularAnimated();
+
+      if (!data || data.length === 0) {
+        setErrorMessage("No Animated Movies found");
+        setAnimatedList([]);
+        return;
+      }
+
+      const validAnimated = data.filter(
+        (anime) => anime && anime.id && (anime.name || anime.title)
+      );
+
+      setAnimatedList(validAnimated);
+
+      if (query && validAnimated.length > 0) {
+        await updateSearchCount(query, validAnimated[0]);
+      }
+    } catch (error) {
+      setErrorMessage("Error fetching movies. Please try again later.");
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchAnimated(debouncedSearchTerm);
+  }, [debouncedSearchTerm, fetchAnimated]);
+
   return (
-    <div>Anime</div>
-  )
+    <section className="all-movies px-24 py-12">
+      <h2>Animated Movies You might like</h2>
+
+      {isLoading ? (
+        <Loading />
+      ) : errorMessage ? (
+        <p className="text-red-500">{errorMessage}</p>
+      ) : (
+        <ul>
+          {animatedList.map((anime) => (
+            <AnimeCard key={anime.id} anime={anime}/>
+          ))}
+        </ul>
+      )}
+    </section>
+  );
 }
--- src/utils/api.js
+++ src/utils/api.js
@@ -39,9 +39,7 @@ export const fetchPopularMovies = async () => {
 
 export const fetchTvSeriesByQuery = async (query) => {
   try {
-    const url = `${API_BASE_URL}/search/tv?query=${encodeURIComponent(
-      query
-    )}`;
+    const url = `${API_BASE_URL}/search/tv?query=${encodeURIComponent(query)}`;
     const res = await fetch(url, API_OPTIONS);
     if (!res.ok) throw new Error("Failed to fetch search results");
     const data = await res.json();
@@ -53,14 +51,48 @@ export const fetchTvSeriesByQuery = async (query) => {
 };
 
 export const fetchPopularTvSeries = async () => {
-    try {
-        const url = `${API_BASE_URL}/discover/tv?sort_by=popularity.desc`;
-        const res = await fetch(url, API_OPTIONS);
-        if (!res.ok) throw new Error("Failed to fetch TV series");
-        const data = await res.json();
-        return data.results;
-    } catch (error) {
-        console.error("Error fetching TV series:", error);
-        return [];
-    }
-}
+  try {
+    const url = `${API_BASE_URL}/discover/tv?sort_by=popularity.desc`;
+    const res = await fetch(url, API_OPTIONS);
+    if (!res.ok) throw new Error("Failed to fetch TV series");
+    const data = await res.json();
+    return data.results;
+  } catch (error) {
+    console.error("Error fetching TV series:", error);
+    return [];
+  }
+};
+
+export const fetchAnimatedByQuery = async (query) => {
+  try {
+    const url = `${API_BASE_URL}/search/movie?query=${encodeURIComponent(
+      query
+    )}&language=en-US`;
+    const res = await fetch(url, API_OPTIONS);
+    if (!res.ok) throw new Error("Failed to fetch animated search results");
+    const data = await res.json();
+
+    // Filter only animation genre (id: 16)
+    const animatedResults = data.results.filter((movie) =>
+      movie.genre_ids.includes(16)
+    );
+
+    return animatedResults;
+  } catch (error) {
+    console.error("Error fetching animated movies by query:", error);
+    return [];
+  }
+};
+
+export const fetchPopularAnimated = async () => {
+  try {
+    const url = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc&with_genres=16&with_keywords=210024`;
+    const res = await fetch(url, API_OPTIONS);
+    if (!res.ok) throw new Error("Failed to fetch TV series");
+    const data = await res.json();
+    return data.results;
+  } catch (error) {
+    console.error("Error fetching TV series:", error);
+    return [];
+  }
+};
